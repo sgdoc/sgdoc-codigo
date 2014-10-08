00de683e0f3e2ab9f2befff53e1916fd1cce3649
Roges Horacio Grandi - SUPDE/DEPAE/DE009 <42902614004@serpro-1543108.(none)>
2014-10-08 17:30:08 -0300

    Atualização da biblioteca nusoap e do testeWebService.

--- extensoes/pr_snas/1.2/webservices/testeWebService.php
+++ extensoes/pr_snas/1.2/webservices/testeWebService.php
@@ -1,12 +1,38 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+$debugNuSoap = 'Nada a declarar';
+
+function imprimir($debug, $orgao, $utf) {
+	echo "\n\nDebug (NuSoapClient) para  o orgao: $orgao, ".($utf ? 'com' : 'sem')." UTF8\n\n";
+	echo $debug;
+	echo "\nFim Debug (NuSoapClient): -------------------------------------------------\n";
+}
+
+function testarErroNuSoap($client) {
+	if ($client->fault) {
+		echo "[ ERRO ]\n";
+		$debugNuSoap = $client->getDebug();
+		throw new Exception(print($result));
+	}
+	$err = $client->getError();
+	if ($err) {
+		echo "[ ERRO ]\n";
+		$debugNuSoap = $client->getDebug();
+		throw new Exception($err);
+	}
+}
+
 echo "Início...\n";
 try {
 	
 	include_once(dirname(__FILE__) . '/include.soap.php');
 	
 	// MDA - Ministério do Desenvolvimento Agrário - MDA
-	$codigoSiorg = '17125';
+	//$codigoSiorg = '17125';
+	//29260
+	$arrCodSiorg = array('17125', '17125', '29260', '29260');
+	//$arrCodSiorg = array('17125');
 	$exercicio = '2014';
 	
 	$configWs = array(
@@ -17,12 +43,6 @@ try {
 		'perfil' 	=> '32'
 	);
 	
-	$orgaosParametros = array(
-		'credencial'	=> array('perfil' => $configWs['perfil'], 'senha' => $configWs['senha'], 'usuario' => $configWs['usuario']),
-		'exercicio'		=> $exercicio,
-		'codigoSiorg'	=> $codigoSiorg
-	);
-	
 	$proxy = array(
 		'server'	=> false,
 		'port'		=> false,
@@ -30,61 +50,60 @@ try {
 		'password'	=> false
 	);
 	
-	echo "Criando cliente para o WebService {$configWs['wsdl_url']}.....";
-	$client = new nusoap_client($configWs['wsdl_url'], $proxy['server'], $proxy['port'], $proxy['username'], $proxy['password'], 0, 3000 );
-	$client->setDebugLevel(9);
-	$client->setUseCURL(true);
+	$bolUtf = false;
 	
-	$err = $client->getError();
-	if ($err) {
-		echo "[ ERRO ]\n";
-		throw new Exception($err);
-	}
-	echo "[ OK ]\n";
-	
-	echo "Chamando o método 'obterOrgaoPorCodigoSiorg(codigoSiorg=$codigoSiorg, exercicio=$exercicio)'.....\n";
-	$result = $client->call('obterOrgaoPorCodigoSiorg', $orgaosParametros, $configWs['namespace']);
-	
-	//print_r($result);exit;
-	
-	echo "Validando o retorno.....";
-	if ($client->fault) {
-		echo "[ ERRO ]\n";
-		throw new Exception(print($result));
-	} else {
-		$err = $client->getError();
-		if ($err) {
-			echo "[ ERRO ]\n";
-			throw new Exception($err);
-		}
-	}
-	echo "[ OK ]\n";
-	
-	echo "Obtendo registros.....";
-	$registros = array();
-	if (array_key_exists('registros', $result)) {
-		if(isset($result['registros'][0])) {
-			$registros = $result['registros'];
+	foreach ($arrCodSiorg as $codigoSiorg) {
+		
+		$orgaosParametros = array(
+			'credencial'	=> array('perfil' => $configWs['perfil'], 'senha' => $configWs['senha'], 'usuario' => $configWs['usuario']),
+			'exercicio'		=> $exercicio,
+			'codigoSiorg'	=> $codigoSiorg
+		);
+		
+		echo "\n\nBuscando o orgao: $codigoSiorg, ".($bolUtf ? 'com' : 'sem')." UTF8\n\n";	
+		echo "Criando cliente para o WebService {$configWs['wsdl_url']}.....";
+		$client = new nusoap_client($configWs['wsdl_url'], $proxy['server'], $proxy['port'], $proxy['username'], $proxy['password'], 0, 3000 );
+		$client->setDebugLevel(9);
+		$client->setUseCURL(true);
+		$client->decode_utf8 = $bolUtf;
+		
+		$bolUtf = !$bolUtf;
+		
+		testarErroNuSoap($client);
+		echo "[ OK ]\n";
+		
+		echo "Chamando o método 'obterOrgaoPorCodigoSiorg(codigoSiorg=$codigoSiorg, exercicio=$exercicio)'.....\n";
+		$result = $client->call('obterOrgaoPorCodigoSiorg', $orgaosParametros, $configWs['namespace']);
+		
+		echo "Validando o retorno.....";
+		testarErroNuSoap($client);
+		echo "[ OK ]\n";
+		
+		echo "Obtendo registros.....";
+		$registros = array();
+		if (array_key_exists('registros', $result)) {
+			if(isset($result['registros'][0])) {
+				$registros = $result['registros'];
+			} else {
+				$registros[0] = $result['registros'];
+			}
+			echo "[ OK ]\nRegistros:\n";
+			foreach ($registros as $reg) {
+				echo "     * {$reg['codigoOrgao']} - {$reg['descricao']}\n";
+			}
+			echo "Fim\n";
+			//exit;
 		} else {
-			$registros[0] = $result['registros'];
+			echo "[ ATENCAO - Não existem registros deste orgao ]\n";
 		}
-		echo "[ OK ]\nRegistros:\n";
-		foreach ($registros as $reg) {
-			echo "     * {$reg['codigoOrgao']} - {$reg['descricao']}\n";
-		}
-		echo "Fim\n";
-		exit;
-	} else {
-		echo "[ ATENCAO - Não existem registros deste orgao ]\n";
+		imprimir($client->getDebug(), $codigoSiorg, $bolUtf);
 	}
-	
-	echo "Debug (NuSoapClient): -----------------------------------------------------\n";
-	echo $client->getDebug();
-	echo "Fim Debug (NuSoapClient): -------------------------------------------------\n";
-	
 } catch (Exception $e) {
 	echo "ERRO na linha {$e->getLine()}: " . $e->getMessage() . "\n";
+	imprimir($debugNuSoap, $codigoSiorg, $bolUtf);
 } catch (SoapFault $ex) {
 	echo "ERRO SOAP na linha {$ex->getLine()}: " . $ex->getMessage() . "\n";
+	imprimir($debugNuSoap, $codigoSiorg, $bolUtf);
 }
+
 echo "Fim\n";
